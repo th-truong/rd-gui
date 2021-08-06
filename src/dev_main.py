5e331e2b34b3e3bab96ae400d661ae9b8199ea86
Thomas Truong <thomas.truong@ucalgary.ca>
2021-08-05 20:37:14 -0600

    tweak training

--- src/dev_main.py
+++ src/dev_main.py
@@ -1,21 +1,22 @@
+import confuse
+from pathlib import Path
+from argparse import Namespace
+from training.data_util.vtranse_dataset import VTranseDataset, VTranseObjDataset, VTranseRelDataset, VTranseRelTrainDataset
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from training.rd_model import rd_full_model
 
 
 def collate_fn(batch):
     return tuple(zip(*batch))
 
 
-if __name__ == "__main__":
-    import confuse
-    from pathlib import Path
-    from argparse import Namespace
-    from training.data_util.vtranse_dataset import VTranseDataset, VTranseObjDataset, VTranseRelDataset, VTranseRelTrainDataset
-    from matplotlib import pyplot as plt
-    from tqdm import tqdm
-    import numpy as np
-    import torch
-    from torch.utils.data import DataLoader
-    from torch.utils.tensorboard import SummaryWriter
-
+def train_rel_model():
     args = Namespace(cfg_path='cfgs/full_model_training_config.yml')
 
     cfg = confuse.Configuration('RD_GUI', __name__, read=False)
@@ -26,8 +27,6 @@ if __name__ == "__main__":
     ds = VTranseRelTrainDataset(dataset_path)
 
     torch_ds = DataLoader(ds, batch_size=1, shuffle=False, num_workers=4, collate_fn=collate_fn)
-
-    from training.rd_model import rd_full_model
 
     full_rd_model_kwargs = cfg['full_rd_model_kwargs'].get()
     model, mrcnn_model = rd_full_model.create_rd_training_models(**full_rd_model_kwargs)
@@ -80,7 +79,7 @@ if __name__ == "__main__":
                     num_correct += 1
                 step += 1
 
-            writer.add_scalar("num_correct", num_correct, step)
+            writer.add_scalar("last_prediction", np.argmax(out.squeeze().cpu().detach().numpy()), step)
             writer.add_scalar("percentage", num_correct/total_labels, step)
         save_path = Path(cfg['tensorboard_path'].get()) / (str(epoch+1) + "_" + str(step) + "_full_epoch.tar")
         torch.save({
@@ -92,3 +91,55 @@ if __name__ == "__main__":
 
     # plt.imshow(out[0][0][0].squeeze())
     #out = model(img)
+
+
+if __name__ == "__main__":
+    train_rel_model()
+    # args = Namespace(cfg_path='cfgs/full_model_training_config.yml')
+
+    # cfg = confuse.Configuration('RD_GUI', __name__, read=False)
+    # cfg.set_file(Path(args.cfg_path))
+    # dataset_path = Path(cfg['visual_genome_path'].get())
+
+    # ds = VTranseRelTrainDataset(dataset_path, ds_set="test")
+    # torch_ds = DataLoader(ds, batch_size=1, shuffle=True, num_workers=4, collate_fn=collate_fn)
+
+    # full_rd_model_kwargs = cfg['full_rd_model_kwargs'].get()
+    # model, mrcnn_model = rd_full_model.create_rd_training_models(**full_rd_model_kwargs)
+    # model.eval()
+    # mrcnn_model.eval()
+
+    # device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    # mrcnn_model.to(device)
+    # model.to(device)
+
+    # saved_model = torch.load(r"D:\paper_repos\rd-gui\src\pretrained_models\first_rel_model\1_660944_full_epoch.tar")
+
+    # model.load_state_dict(saved_model['model'])
+    # for _ in tqdm(torch_ds):
+    #     img, targets, sub_inputs, obj_inputs = _
+    #     img = [x.to(device) for x in img]
+    #     targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+    #     # iterates through the tuple for batch size and then the list to convert todevice
+
+    #     for i, y in enumerate(sub_inputs):
+    #         for j, x in enumerate(y):
+    #             sub_inputs[i][j] = sub_inputs[i][j].to(device)
+    #     for i, y in enumerate(obj_inputs):
+    #         for j, x in enumerate(y):
+    #             obj_inputs[i][j] = obj_inputs[i][j].to(device)
+    #     with torch.no_grad():
+    #         losses, detections, features = mrcnn_model(img)
+    #     for i in range(len(targets[0]['rel_labels'])):
+    #         # careful with inputs, sub_inputs and obj_inputs should be a list. if slicing remove the parenthesese
+    #         out = model(features, targets, [sub_inputs[0][i]], [obj_inputs[0][i]])
+
+    #         print("-----------------------------------------")
+    #         print("preds")
+    #         print(np.argmax(out.cpu().detach().numpy()))
+    #         print(np.max(out.cpu().detach().numpy()))
+    #         print("true")
+    #         print(np.argmax(targets[0]['rel_labels'][i].cpu().numpy()))
+    #         print(np.max(targets[0]['rel_labels'][i].cpu().numpy()))
+    #         plt.imshow(np.moveaxis(img[0].cpu().squeeze().numpy(), 0, -1))
+    #     break
